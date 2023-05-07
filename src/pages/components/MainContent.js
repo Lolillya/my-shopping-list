3846ba55a95b37721b6b4e1b87bea2944613ab25
Lolillya <SomeonesUsernam@gmail.com>
2023-05-07 21:38:56 +0800

    2nd update, added checked feature

--- src/pages/components/MainContent.js
+++ src/pages/components/MainContent.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { nanoid } from 'nanoid'
 
 import 'firebase/firestore'
 import 'firebase/auth'
 
 import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, push, onValue, remove } from "firebase/database"
+import { getDatabase, ref, push, onValue, remove, set, update } from "firebase/database"
+
 
 export default function MainContent() {
     //------------------------------------------------------
@@ -16,33 +18,50 @@ export default function MainContent() {
     }
 
     const app = initializeApp(appSettings)
-    const database = getDatabase(app)
-    const shoppingListDB = ref(database, "shoppingList")
+    const database = getDatabase()
+    const shoppingListDB = ref(database, `shoppingList/`)
+
 
     //-------------------------------------------------------
     //-------------------------------------------------------
     //-------------------------------------------------------
-
 
     const [value, setValue] = React.useState("")
     const [shopList, setShopList] = React.useState([])
     const [hasValue, setHasValue] = React.useState()
 
     const shopListElements = shopList.map(list =>
-        <li id='list' key={list[0]}>
-            <p id="item--name">{list[1]}</p>
+        <li id='list' key={list.key} style={list.checked ? {backgroundColor: "#b4f7a3"} : {}}>
 
-            <div id="selection--container">
-                <div id="check_Box">
-                    /
+            <section id="itembox--container">
+                <p id="item--name">{list.name}</p>
+                <div id="selection--container">
+                    <div id="check_Box" onClick={() => checkItem(list.key, list.checked)}>
+                        /
+                    </div>
+                    <div id="X_Box" onClick={() => removeItem(list.key)}>
+                        X
+                    </div>
                 </div>
-                <div id="X_Box" onClick={() => removeItem(list[0])}>
-                    X
+            </section>
+
+            <section className="alt--sidenote--container">
+                <div id="alt">
+                    <h5 style={{ marginBottom: "10px" }}>Alternatives</h5>
+                    <h6 style={{ margin: 0 }}>No alternatives listed.</h6>
                 </div>
-            </div>
-            <div rowspan={2} id="list--dropdown--button">
+
+                <div id="sidenote">
+                    <h5 style={{ marginBottom: "10px" }}>Sidenotes</h5>
+                    <h6 style={{ margin: 0 }}>No sidenotes posted</h6>
+                </div>
+            </section>
+
+            <div id="list--dropdown--button">
                 ...
             </div>
+
+
         </li>
     )
 
@@ -50,13 +69,23 @@ export default function MainContent() {
         onValue(shoppingListDB, function (myDBList) {
             if (myDBList.exists()) {
                 setHasValue(myDBList.exists())
-                let ls = Object.entries(myDBList.val())
+
+                let ls = Object.values(myDBList.val())
                 setShopList(ls)
             }
             else
                 setHasValue(false)
         })
     }, [])
+
+    function checkItem(key, checked) {
+        // let loc = ref(database, `shoppingList/${key}`)
+        update(ref(database, 'shoppingList/' + key), {
+            checked: !checked
+        })
+        
+        // console.log(loc)
+    }
 
     function removeItem(key) {
         let exactDBLoc = ref(database, `shoppingList/${key}`)
@@ -68,7 +97,13 @@ export default function MainContent() {
     }
 
     function handleClick() {
-        push(shoppingListDB, value)
+        let key = nanoid();
+
+        set(ref(database, "shoppingList/" + key), {
+            name: value,
+            key: key,
+            checked: false
+        })
         setValue("")
     }
     return (
@@ -77,7 +112,7 @@ export default function MainContent() {
                 <ul className='shop-list'>
                     {hasValue && shopListElements}
                 </ul>
-                {!hasValue && <h3 style={{ alignSelf: 'center' }}>No items added</h3>}
+                {!hasValue && <h3 style={{ justifyContent: 'center' }}>No items added</h3>}
             </section>
 
             <section className='input--container'>
